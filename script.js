ad37a21c4dbf2c42496cd3fc8183b89399539e2c
Igor Nefedov <102536675+espirapaco23@users.noreply.github.com>
2023-02-22 02:47:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+//change background header
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
+
+//scrollup button
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
+
+// scroll sections active link
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute("id");
+    const sectionsClass = document.querySelector(
+      ".nav__menu a[href*=" + sectionId + "]"
+    );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+//////////scrollanimation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+
+sr.reveal(`.home__data, .products__data, .steps__content, .footer__container`);
+sr.reveal(`.home__img`, { origin: "bottom" });
+sr.reveal(`.products__card`, { interval: 100 });
+sr.reveal(`.about__img, .testimonial__img`, { origin: "right" });
+sr.reveal(`.about__data, .testimonial__data`, { origin: "left" });
